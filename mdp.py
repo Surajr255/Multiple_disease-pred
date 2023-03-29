44e1a0b33a90f5a52f681cd63b1d0856dab2abda
Suraj Chaudhary <57636161+Surajr255@users.noreply.github.com>
2023-03-30 01:55:50 +0545

    mdp.py

--- mdp.py
+++ mdp.py
@@ -33,7 +33,7 @@ with st.sidebar:
                           ['Diabetes Prediction',
                            'Heart Disease Prediction',
                            'Parkinsons Prediction','Breast Cancer Prediction'],
-                          icons=['activity','heart','person','Hospital'],
+                          icons=['activity','heart','person','hospital'],
                           default_index=0)
     
     
