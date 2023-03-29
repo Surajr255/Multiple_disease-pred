a8faa1670114215df41136aea2c21e515bcc83c0
Suraj Chaudhary <57636161+Surajr255@users.noreply.github.com>
2023-03-30 01:54:21 +0545

    mdp.py

--- mdp.py
+++ mdp.py
@@ -32,8 +32,8 @@ with st.sidebar:
                           
                           ['Diabetes Prediction',
                            'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
+                           'Parkinsons Prediction','Breast Cancer Prediction'],
+                          icons=['activity','heart','person','Hospital'],
                           default_index=0)
     
     
@@ -249,73 +249,9 @@ if (selected == "Parkinsons Prediction"):
     st.success(parkinsons_diagnosis)
 
 
+
 # Breast Cancer Prediction
+if (selected == "Breast Cancer Prediction"):
 
-def home():
-    st.write('''
-    # Breast Cancer Prediction
-    This app predicts the likelihood of breast cancer based on various features.
-    To get started, select a feature on the left sidebar.
-    ''')
-def breast_cancer_prediction():
-    # Load the model
-    bc_model = pickle.load(open('breast_cancer_model.sav', 'rb'))
-    
-    # Get inputs from the user
-    radius_mean = st.number_input("Radius Mean", value=14.0)
-    texture_mean = st.number_input("Texture Mean", value=20.0)
-    perimeter_mean = st.number_input("Perimeter Mean", value=91.0)
-    area_mean = st.number_input("Area Mean", value=630.0)
-    smoothness_mean = st.number_input("Smoothness Mean", value=0.1)
-    compactness_mean = st.number_input("Compactness Mean", value=0.1)
-    concavity_mean = st.number_input("Concavity Mean", value=0.1)
-    concave_points_mean = st.number_input("Concave Points Mean", value=0.1)
-    symmetry_mean = st.number_input("Symmetry Mean", value=0.1)
-    fractal_dimension_mean = st.number_input("Fractal Dimension Mean", value=0.1)
-    radius_se = st.number_input("Radius SE", value=0.4)
-    texture_se = st.number_input("Texture SE", value=1.0)
-    perimeter_se = st.number_input("Perimeter SE", value=4.0)
-    area_se = st.number_input("Area SE", value=20.0)
-    smoothness_se = st.number_input("Smoothness SE", value=0.01)
-    compactness_se = st.number_input("Compactness SE", value=0.02)
-    concavity_se = st.number_input("Concavity SE", value=0.02)
-    concave_points_se = st.number_input("Concave Points SE", value=0.01)
-    symmetry_se = st.number_input("Symmetry SE", value=0.01)
-    fractal_dimension_se = st.number_input("Fractal Dimension SE", value=0.002)
-    radius_worst = st.number_input("Radius Worst", value=17.0)
-    texture_worst = st.number_input("Texture Worst", value=29.0)
-    perimeter_worst = st.number_input("Perimeter Worst", value=114.0)
-    area_worst = st.number_input("Area Worst", value=925.0)
-    smoothness_worst = st.number_input("Smoothness Worst", value=0.13)
-    compactness_worst = st.number_input("Compactness Worst", value=0.3)
-    concavity_worst = st.number_input("Concavity Worst", value=0.3)
-    concave_points_worst = st.number_input("Concave Points Worst", value=0.1)
-    symmetry_worst = st.number_input("Symmetry Worst", value=0.2)
-    fractal_dimension_worst = st.number_input("Fractal Dimension Worst", value=0.05)
-
-    # Make prediction
-    prediction = bc_model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]])
-
-    # Display the prediction
-    st.write("")
-    if prediction == 0:
-        st.warning('The patient is likely to be benign (not have breast cancer)')
-    else:
-        st.success('The patient is likely to be malignant (have breast cancer)')
-
-
-# Sidebar Navigation
-menu = ['Home', 'Parkinsons Disease', 'Diabetes', 'Heart Disease', 'Breast Cancer']
-choice = st.sidebar.selectbox('Select the Disease you want to predict', menu)
-
-# Display the appropriate page based on the user's menu choice
-if choice == 'Home':
-    home()
-elif choice == 'Parkinsons Disease':
-    parkinsons_prediction()
-elif choice == 'Diabetes':
-    diabetes_prediction()
-elif choice == 'Heart Disease':
-    heart_disease_prediction()
-elif choice == 'Breast Cancer':
-    breast_cancer_prediction()
+  # page title
+    st.title('Breast Cancer Prediction using machine learning')
